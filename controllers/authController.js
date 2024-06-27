18b1d2c780c858cf3a6029bba723be972149751a
Ishaan1764 <ishaansaluja1764@gmail.com>
2024-06-27 15:33:22 -0400

    created login route (next step-> to create login middleware)

--- controllers/authController.js
+++ controllers/authController.js
@@ -42,3 +42,20 @@ module.exports.registerUser=async function (req,res){
     }
     
 }
+
+module.exports.loginUser=async function(req,res){
+    let{email,password}=req.body;
+
+    let user=await userModel.findOne({email:email});
+    if(!user) return res.send("Email password Incorrect");
+    
+    bcrypt.compare(password,user.password,function(err,result){
+        if(result){
+            let token=generateToken(user);
+            res.cookie("token",token);
+            res.send("you can login");
+        }else{
+            return res.send("PAssword Incorrect!!")
+        }
+    });
+}
